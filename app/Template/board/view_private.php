4b9560c76b0cd02a05bc0dae20ae1d6f6605b17b
Y.Okamoto <yohei.okamoto@gmail.com>
2021-08-25 15:24:02 +0900

    BoardViewへattachmentフィールドを追加してみた。

--- app/Template/board/view_private.php
+++ app/Template/board/view_private.php
@@ -1,5 +1,13 @@
 <section id="main">
+    <!--成果物の欄 -->
+    // TODO: グローバルタスクのようにプロジェクトに紐づくタスクの登録＆それを引用するように修正する
 
+    <?= $this->hook->render('template:task:show:before-attachments', array('task' => $task, 'project' => $project)) ?>
+    <?= $this->render('task_file/show', array(
+        'task' => $task,
+        'files' => $files,
+        'images' => $images
+    )) ?>
     <?= $this->projectHeader->render($project, 'BoardViewController', 'show', true) ?>
 
     <?= $this->render('board/table_container', array(
